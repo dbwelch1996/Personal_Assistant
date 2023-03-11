a91b64636869039962874e4cb8172d7f4bcbe98a
Dylan Welch <welch.dylan1996@gmail.com>
2023-03-11 15:46:51 -0800

    basic voice

--- PersonalAssistant.py
+++ PersonalAssistant.py
@@ -1,1 +1,9 @@
-print("hello")
+import gradio as gr
+
+def transcribe(audio):
+    print(audio)
+    return "transcription goes here"
+
+ui = gr.Interface(fn =transcribe, inputs = gr.Audio(source = "microphone", type = "filepath"), outputs = "text")
+
+ui.launch()
