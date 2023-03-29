13571905a4fc7e88349c7d9dcc70308a657e55b5
Dylan Welch <welch.dylan1996@gmail.com>
2023-03-29 12:38:58 -0700

    added GPT function, allows for transcript/response

--- PersonalAssistant.py
+++ PersonalAssistant.py
@@ -4,9 +4,31 @@ import config
 
 openai.api_key = config.API_KEY
 
+
+conversations =  [{"role": "system", "content": "You are a helpful assistant who speaks like donald trump."},
+
+]
+
+
 def transcribe(audio):
+    
+    global conversations
     print(audio)
-    return "transcription goes here"
+    audio_file = open (audio, "rb")
+    
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    conversations.append({"role": "user", "content": transcript["text"]})
+    
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversations)
+    
+    system_message = response["choices"][0]["content"] 
+    conversations.append({"role": "assistant", "content": system_message})  
+    
+    #19:01
+     
+    print(response)
+    return transcript["text"]
+    
 
 ui = gr.Interface(fn =transcribe, inputs = gr.Audio(source = "microphone", type = "filepath"), outputs = "text")
 
